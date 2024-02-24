fe2aca0eceba9949866c88b6acfae3f91ee7b0d6
mish3mish <mishakostiuchenko@gmail.com>
2024-02-24 16:11:02 -0500

    update1

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+function getElementWidth(content, padding, border) {
+
+    // const contentWidth = parseFloat(content);
+    // const paddingWidth = parseFloat(padding);
+    // const borderWidth = parseFloat(border);
+
+    // const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+    // return totalWidth;
+  
+  return (Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2);
+    
+}
+  
+ 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+  
+  
